f90c3cb1c21e219b8a86a2058d506a68266bf10a
Lakshya Yadav <lakmtj@gmail.com>
2022-01-27 16:20:41 +0530

    simon game added

---
+++ jquery/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,7 @@
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
